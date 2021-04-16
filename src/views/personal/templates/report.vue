c5b214ded1fc3a87b654679cda2209407606df26
peter <980028458@qq.com>
2021-04-16 17:04:52 +0800

    4.16

--- src/views/personal/templates/report.vue
+++ src/views/personal/templates/report.vue
@@ -1,42 +1,72 @@
 <template>
   <div class="contier">
-    <el-table
-      :data="tableData"
-      stripe
-      :header-cell-style="{background:'#E5E9EF',color:'#151C2C'}"
-      class="tables"
-      height="480"
-    >
-      <el-table-column prop="id" label="报告id"></el-table-column>
-      <el-table-column prop="reportNo" label="报告编码" width="160">
-        <template slot-scope="{ row }">{{row.reportNo | getString}}</template>
-      </el-table-column>
-      <el-table-column prop="reportType" label="报告类型">
-        <template slot-scope="{ row }">{{row.reportType | getReportType}}</template>
-      </el-table-column>
-      <el-table-column prop="reportStatus" label="报告状态">
-        <template slot-scope="{ row }">{{row.reportStatus | getReportStatus}}</template>
-      </el-table-column>
-      <el-table-column prop="paidStatus" label="支付状态">
-        <template slot-scope="{ row }">{{row.paidStatus | getPaidStatus}}</template>
-      </el-table-column>
-      <el-table-column prop="createTime" label="创建时间" width="160"></el-table-column>
-      <el-table-column prop="updateTime" label="更新时间"></el-table-column>
-      <el-table-column label="操作" fixed="right" width="50">
-        <template slot-scope="{row}">
-          <el-button type="text" @click="downLoadReport(row)">下载</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <Pagination
-      v-show="total > 0"
-      :total="total"
-      layout="prev, pager, next"
-      :page.sync="params.pageNo"
-      :limit.sync="params.pageSize"
-      @pagination="fetchData"
-      class="page"
-    />
+    <div v-if="active !== 0" style="color: rgb(102, 102, 102);padding-left: 20px;margin-bottom: 20px;cursor: pointer;">
+      <span @click="select({id: 0})" class="management">报告管理</span> /
+      <span>{{selectFile}}</span>
+    </div>
+    <div v-else style="display: flex;padding:0 25px 0 10px;margin-bottom: 5px;justify-content: space-between;align-items: center;">
+      <div style="display: flex;">
+        <div v-for="item in data" @click="select(item)" :key="item.id" @dragleave="dragleave" class="group" @dragover="allowDrop" @drop="drop">
+          <div>
+            <img src="../../../assets/files.png" width="30" alt="">
+          </div>
+          {{item.name}}
+        </div>
+      </div>
+      <div>
+        <el-button type="primary" @click="dialogVisible = true">添加</el-button>
+      </div>
+    </div>
+    <div>
+      <el-table
+        :data="tableData"
+        stripe
+        :header-cell-style="{background:'#E5E9EF',color:'#151C2C'}"
+        class="tables"
+        height="480"
+      >
+        <el-table-column prop="id" label="报告id"></el-table-column>
+        <el-table-column prop="reportNo" label="报告编码" width="160">
+          <template slot-scope="{ row }">{{row.reportNo | getString}}</template>
+        </el-table-column>
+        <el-table-column prop="reportType" label="报告类型">
+          <template slot-scope="{ row }">{{row.reportType | getReportType}}</template>
+        </el-table-column>
+        <el-table-column prop="reportStatus" label="报告状态">
+          <template slot-scope="{ row }">{{row.reportStatus | getReportStatus}}</template>
+        </el-table-column>
+        <el-table-column prop="paidStatus" label="支付状态">
+          <template slot-scope="{ row }">{{row.paidStatus | getPaidStatus}}</template>
+        </el-table-column>
+        <el-table-column prop="createTime" label="创建时间" width="160"></el-table-column>
+        <el-table-column prop="updateTime" label="更新时间"></el-table-column>
+        <el-table-column label="操作" fixed="right" width="50">
+          <template slot-scope="{row}">
+            <el-button type="text" @click="downLoadReport(row)">下载</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <Pagination
+        v-show="total > 0"
+        :total="total"
+        layout="prev, pager, next"
+        :page.sync="params.pageNo"
+        :limit.sync="params.pageSize"
+        @pagination="fetchData"
+        class="page"
+      />
+    </div>
+
+    <el-dialog
+      title="新建文件夹"
+      :visible.sync="dialogVisible"
+      width="30%">
+      <el-input />
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -79,12 +109,24 @@ export default {
   data() {
     return {
       total: 0,
+      dialogVisible: false,
+      active: 0,
       params: {
         pageNo: 1,
         pageSize: 10
       },
-      tableData: []
+      tableData: [],
+      data: [
+        { id: 1, name: '文件夹1' },
+        { id: 2, name: '文件夹2' },
+        { id: 3, name: '文件夹3' },
+      ]
     };
+  },
+  computed: {
+    selectFile() {
+      return this.data.find(e => e.id === this.active).name
+    }
   },
   components: {
     Pagination: Pagination
@@ -93,11 +135,38 @@ export default {
     this.fetchData();
   },
   methods: {
+    dragleave(ev) {
+      ev.target.classList.remove('active');
+    },
+    select(item) {
+      this.active = item.id
+    },
+    allowDrop(ev) {
+      ev.target.classList.add('active');
+      ev.preventDefault();
+    },
+    drop(ev) {
+      ev.preventDefault();
+      // var data = ev.dataTransfer.getData("Text");
+      ev.target.classList.remove('active');
+    },
     fetchData() {
       Api_person.getReportList(this.params).then(res => {
         console.log(res);
         this.tableData = res.data.list;
         this.total = res.data.total - 0;
+        this.$nextTick(() => {
+          const tr = document.getElementsByTagName('tr')
+          tr.forEach(e => {
+            if (Array.from(e.classList).includes('el-table__row')) {
+              e.setAttribute('draggable', true)
+              e.addEventListener('dragstart', (ev) => {
+                ev.target.id = 'tr' + Math.random()
+                ev.dataTransfer.setData("Text", ev.target.id);
+              })
+            }
+          })
+        })
       });
     },
     // 下载报告
@@ -141,5 +210,24 @@ export default {
 }
 /deep/ .cell {
   font-size: 16px;
+}
+.group {
+  width: 130px; 
+  height:50px;
+  display: flex;
+  font-size: 16px;
+  color: #333;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.group img{
+  margin-right: 15px;
+}
+.management:hover{
+  color: rgb(0, 116, 248);
+} 
+.active{
+  opacity: 0.5;
 }
 </style>
